87d1f9fd403b2d031afa2fb8d381c0592d0be330
jgc <Administrator@ND--20160713ZIC>
2017-02-04 16:20:36 +0800

    整合redis

---
+++ src/main/java/com/nd/spring/dao/ValueRedisDao.java
@@ -0,0 +1,22 @@
+package com.nd.spring.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ValueRedisDao {
+    @Autowired
+    public RedisBaseDao redisBaseDao;
+
+    private String getKey() {
+        return "param";
+    }
+
+    public void save(String param) {
+        this.redisBaseDao.addValue(this.getKey(), param);
+    }
+
+    public String getParam() {
+        return this.redisBaseDao.getValue(this.getKey());
+    }
+}
---
+++ src/test/java/com/nd/spring/RedisTest.java
@@ -0,0 +1,23 @@
+package com.nd.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nd.spring.controller.AuthorController;
+import com.nd.spring.dao.ValueRedisDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AuthorController.class)
+public class RedisTest {
+    @Autowired
+    private ValueRedisDao valueRedisDao;
+ 
+    @Test
+    public void test() throws Exception {
+    this.valueRedisDao.save("cc");
+    System.out.println(this.valueRedisDao.getParam());
+    }
+}
